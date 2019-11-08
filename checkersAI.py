95481d06ede25a8d151ce7494c040624aea7dc80
pujol99 <33929967+pujol99@users.noreply.github.com>
2019-11-08 17:52:13 +0100

    Update checkersAI.py

--- checkersAI.py
+++ checkersAI.py
@@ -24,19 +24,19 @@ BOARD = [
 ]
 
 #IMAGES
-ORANGE = pygame.image.load(os.path.abspath('orange.png'))
-BROWN = pygame.image.load(os.path.abspath('brown.png')
-ORANGEQ = pygame.image.load(os.path.abspath('orangeQ.png')
-BROWNQ = pygame.image.load(os.path.abspath('brownQ.png')
-SELECTED_END = pygame.image.load(os.path.abspath('selected_end.png')
-SELECTED_KILL = pygame.image.load(os.path.abspath('selected_kill.png')
-SELECTED_ORIGN = pygame.image.load(os.path.abspath('selected_orign.png')
-BOARD_IMG = pygame.image.load(os.path.abspath('board.png')
-PREV = pygame.image.load(os.path.abspath('prev.png')
-HINT = pygame.image.load(os.path.abspath('hint.png')
-LAST = pygame.image.load(os.path.abspath('last.png')
-AGAIN = pygame.image.load(os.path.abspath('again.png')
-FRONTPAGE = pygame.image.load(os.path.abspath('frontpage.png')
+ORANGE = pygame.image.load(os.path.abspath('imgs/orange.png'))
+BROWN = pygame.image.load(os.path.abspath('imgs/brown.png'))
+ORANGEQ = pygame.image.load(os.path.abspath('imgs/orangeQ.png'))
+BROWNQ = pygame.image.load(os.path.abspath('imgs/brownQ.png'))
+SELECTED_END = pygame.image.load(os.path.abspath('imgs/selected_end.png'))
+SELECTED_KILL = pygame.image.load(os.path.abspath('imgs/selected_kill.png'))
+SELECTED_ORIGN = pygame.image.load(os.path.abspath('imgs/selected_orign.png'))
+BOARD_IMG = pygame.image.load(os.path.abspath('imgs/board.png'))
+PREV = pygame.image.load(os.path.abspath('imgs/prev.png'))
+HINT = pygame.image.load(os.path.abspath('imgs/hint.png'))
+LAST = pygame.image.load(os.path.abspath('imgs/last.png'))
+AGAIN = pygame.image.load(os.path.abspath('imgs/again.png'))
+FRONTPAGE = pygame.image.load(os.path.abspath('imgs/frontpage.png'))
 
 #COLORS
 BLUE = pygame.Color('0x4363d8')
